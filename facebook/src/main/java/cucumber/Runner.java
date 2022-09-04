9a2644b20cd21dde87e880c6ac1098f0e4c4b418
EPAM\Madhavi_Akula <Madhavi.Akula@cantire.com>
2022-09-04 21:27:38 +0530

    facebook page automation

---
+++ facebook/src/main/java/cucumber/Runner.java
@@ -0,0 +1,9 @@
+package cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+@CucumberOptions(features="src/test/resources",
+				glue="stepdefinitions")
+public class Runner extends AbstractTestNGCucumberTests
+{
+}
+	
