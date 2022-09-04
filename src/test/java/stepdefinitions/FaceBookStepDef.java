437723060abc77a4cc110594f922fcdc3aceac9c
EPAM\Madhavi_Akula <Madhavi.Akula@cantire.com>
2022-09-04 22:38:31 +0530

    Setting java version

--- src/test/java/stepdefinitions/FaceBookStepDef.java
+++ src/test/java/stepdefinitions/FaceBookStepDef.java
@@ -2,6 +2,8 @@ package stepdefinitions;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 import com.epam.pom.HomePage;
 import com.epam.pom.SigninPage;
 import io.cucumber.java.After;
@@ -17,6 +19,7 @@ public class FaceBookStepDef {
 	HomePage homepage;
 
 
+	@Test(priority = 1)
 	@Given("i open the facebook page")
 	public void i_open_the_browser() {
 		WebDriverManager.chromedriver().setup();
@@ -24,21 +27,25 @@ public class FaceBookStepDef {
 		SigninPage.openBrowser(driver);
 	}
 
+	@Test(priority = 2)
 	@And("i Enter username and password")
 	public void enter_login_details() {
 		SigninPage.loginDetails(driver);
 	}
-
+	
+	@Test(priority = 3)
 	@When("I click on login button")
 	public void click_login() {
 		SigninPage.clickOnLogin(driver);
 	}
 	
-	
+	@Test(priority = 4)
     @Then("I want to see the home page")
 	public void see_home_page() {
     	HomePage.homePageShouldBevisible(driver);
 	}
+	
+	@Test(priority = 4)
     @Then("I want to search the person in home page")
    	public void search_person() {
        	HomePage.enterTextInsearchbar(driver);
